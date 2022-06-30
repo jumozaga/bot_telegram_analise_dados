4a44870f3cb54183f00d406e35c61b97df434217
jumozaga <jumozaga@yahoo.com>
2022-06-29 22:44:31 -0300

    Projeto Bot telegram Analise de dados v1

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.telegramBot import TelegramBot
+
+# Acessa a classe criada
+bot = TelegramBot()
+
+# Executa o método criado (função)
+bot.start()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bot de Telegram que entrega análise de dados para RH (dados ficticios)recebidos por uma planilha gerada pelos funcionarios com seus feedbacks. [D[D[D',
+    author='jumozaga',
+    license='',
+)
